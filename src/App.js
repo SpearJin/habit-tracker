c282357d852e2623ca7e83358851aa7b3eb5faea
park-chang-jin <49690383+park-chang-jin@users.noreply.github.com>
2021-08-05 23:45:57 +0900

    habit: HTML

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Habit from "./components/habit";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Habit />
+    </div>
+  );
 }
 
 export default App;
